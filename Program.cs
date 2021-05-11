6b8d228cf8fcf723c57c3b654c6ef3b7760955cf
LWooler <lwooler@att.net>
2021-05-11 14:25:21 -0500

    created new class vehicle and extended classes car and semi

--- Program.cs
+++ Program.cs
@@ -6,7 +6,35 @@ namespace VehicleAccessors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car newCar = new Car();
+            Semi newCar2 = new Semi();
         }
+    }
+
+    public class Vehicle
+    {
+        protected string model;
+        protected string make;
+        protected string vinNumber;
+
+        protected Vehicle() {
+
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public Car() {
+
+        }
+
+    }
+
+    public class Semi : Vehicle
+    {
+        public Semi() {
+
+        }
+
     }
 }
